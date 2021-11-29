463bd646451335ca3177f17f1a35aca485758547
Aidan Morgan <atmorga3@asu.edu>
2021-11-29 16:12:55 -0700

    Revert "Merge branch 'main' of https://github.com/atmorga3/CSE412FinalProject"
    This reverts commit c3fa8ba6e2dc3969cbe58427eda32b10a738ba9b, reversing
    changes made to 34d8b5cf37cce932ee28aad16949128554ff800f.

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
-headings = ("h1", "h2", "h3")
-data = (
-    ("r1", "r1", "r1"),
-    ("r2", "r2", "r2"),
-    ("r3", "r3", "r3")
-)
-
 @app.route('/')
 def index():
-    return render_template('index.html', headings=headings, data=data)
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
