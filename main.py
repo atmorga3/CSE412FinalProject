c174e310fbb682640c458e40fc36d2d6569ffa48
kevin-park01 <jkphyke08@gmail.com>
2021-11-29 16:08:00 -0700

    Added Table

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+headings = ("h1", "h2", "h3")
+data = (
+    ("r1", "r1", "r1"),
+    ("r2", "r2", "r2"),
+    ("r3", "r3", "r3")
+)
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', headings=headings, data=data)
 
 if __name__ == '__main__':
     app.run()
