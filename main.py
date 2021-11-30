0417662aeaea78f0a5a29df7df32c74776afdedf
kevin-park01 <jkphyke08@gmail.com>
2021-11-29 19:45:46 -0700

    Added Database Access

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-headings = ("h1", "h2", "h3")
-data = (
-    ("r1", "r1", "r1"),
-    ("r2", "r2", "r2"),
-    ("r3", "r3", "r3")
-)
+# Access database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:pass@localhost/postgres'
+db = SQLAlchemy(app)
+
+# Get all tables
+ban = db.Table('ban', db.metadata, autoload=True, autoload_with=db.engine)
+champion = db.Table('champion', db.metadata, autoload=True, autoload_with=db.engine)
+match = db.Table('match', db.metadata, autoload=True, autoload_with=db.engine)
+player = db.Table('player', db.metadata, autoload=True, autoload_with=db.engine)
+plays = db.Table('plays', db.metadata, autoload=True, autoload_with=db.engine)
+
+# Headers and data to fill table
+headings = list()
+data = list()
 
 @app.route('/')
 def index():
+    headings = ("Champion ID", "Champion Name")
+    results = db.session.query(champion).all()
+    for i in results:
+        data.append((i.champion_id, i.champion_name))
     return render_template('index.html', headings=headings, data=data)
 
 if __name__ == '__main__':
