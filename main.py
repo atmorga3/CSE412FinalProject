17509589586ffe0d9902e797181bdbb5ab92c3eb
kevin-park01 <jkphyke08@gmail.com>
2021-11-30 22:06:09 -0700

    Dropdown and Backend Functions

--- main.py
+++ main.py
@@ -14,16 +14,50 @@ match = db.Table('match', db.metadata, autoload=True, autoload_with=db.engine)
 player = db.Table('player', db.metadata, autoload=True, autoload_with=db.engine)
 plays = db.Table('plays', db.metadata, autoload=True, autoload_with=db.engine)
 
+# Execute a query and return results
+def executeQuery(query):
+    return db.session.execute(query)
+
+# Get headers
+def getBanHeader():
+    return executeQuery('SELECT * FROM ban').keys()
+
+def getChampionHeader():
+    return executeQuery('SELECT * FROM champion').keys()
+
+def getMatchHeader():
+    return executeQuery('SELECT * FROM match').keys()
+
+def getPlayerHeader():
+    return executeQuery('SELECT * FROM player').keys()
+
+def getPlaysHeader():
+    return executeQuery('SELECT * FROM plays').keys()
+
+# Get data
+def getBanData():
+    return db.session.query(ban).all()
+
+def getChampionData():
+    return db.session.query(champion).all()
+
+def getMatchData():
+    return db.session.query(match).all()
+
+def getPlayerData():
+    return db.session.query(player).all()
+
+def getPlaysData():
+    return db.session.query(plays).all()
+
 # Headers and data to fill table
 headings = list()
 data = list()
 
 @app.route('/')
 def index():
-    headings = ("Champion ID", "Champion Name")
-    results = db.session.query(champion).all()
-    for i in results:
-        data.append((i.champion_id, i.champion_name))
+    headings = getChampionHeader()
+    data = getChampionData()
     return render_template('index.html', headings=headings, data=data)
 
 if __name__ == '__main__':
